11c75e40edc9281bf2e2c811fb3b830bb9ced70d
Tiger Nie <nhl0819@gmail.com>
2021-01-29 04:35:55 +0800

    changed netloc to hostname to be consistent with stdlib

--- proj1/urlparse.py
+++ proj1/urlparse.py
@@ -3,7 +3,7 @@ from typing import NamedTuple, Optional
 
 class URL(NamedTuple):
     scheme: str  # "http"
-    netloc: str  # "www.python.org"
+    hostname: str  # "www.python.org"
     port: Optional[int]  # 80
     path: str  # "/3/library"
     # params: str
@@ -25,10 +25,10 @@ def urlparse(raw: str) -> URL:
         scheme = raw[:i]
         raw = raw[i + 3 :]
 
-    # netloc and port
+    # hostname and port
     i = raw.find(":")
     if i != -1:  # port present
-        netloc = raw[:i]
+        hostname = raw[:i]
         raw = raw[i + 1 :]
 
         i = raw.find("/")
@@ -43,17 +43,17 @@ def urlparse(raw: str) -> URL:
 
         i = raw.find("/")
         if i == -1:
-            netloc = raw
+            hostname = raw
             path = "/"
         else:
-            netloc = raw[:i]
+            hostname = raw[:i]
             path = raw[i:]
 
     # ignoring query and fragment for now
 
     return URL(
         scheme=scheme,
-        netloc=netloc,
+        hostname=hostname,
         port=port,
         path=path,
     )
@@ -67,7 +67,7 @@ if __name__ == "__main__":
             raw = "http://www.cwi.nl:80/%7Eguido/Python.html"
             url = urlparse(raw)
             self.assertEqual(url.scheme, "http", "Incorrect scheme")
-            self.assertEqual(url.netloc, "www.cwi.nl", "Incorrect netloc")
+            self.assertEqual(url.hostname, "www.cwi.nl", "Incorrect hostname")
             self.assertEqual(url.port, 80, "Incorrect port")
             self.assertEqual(url.path, "/%7Eguido/Python.html", "Incorrect path")
 
@@ -75,7 +75,7 @@ if __name__ == "__main__":
             raw = "insecure.stevetarzia.com"
             url = urlparse(raw)
             self.assertEqual(url.scheme, "", "Incorrect scheme")
-            self.assertEqual(url.netloc, "insecure.stevetarzia.com", "Incorrect netloc")
+            self.assertEqual(url.hostname, "insecure.stevetarzia.com", "Incorrect hostname")
             self.assertEqual(url.port, 80, "Incorrect port")
             self.assertEqual(url.path, "/", "Incorrect path")
 
@@ -83,7 +83,7 @@ if __name__ == "__main__":
             raw = "http://somewebsite.com/path/page.htm"
             url = urlparse(raw)
             self.assertEqual(url.scheme, "http", "Incorrect scheme")
-            self.assertEqual(url.netloc, "somewebsite.com", "Incorrect netloc")
+            self.assertEqual(url.hostname, "somewebsite.com", "Incorrect hostname")
             self.assertEqual(url.port, 80, "Incorrect port")
             self.assertEqual(url.path, "/path/page.htm", "Incorrect path")
 
@@ -91,7 +91,7 @@ if __name__ == "__main__":
             raw = "https://www.google.com:443/maps"
             url = urlparse(raw)
             self.assertEqual(url.scheme, "https", "Incorrect scheme")
-            self.assertEqual(url.netloc, "www.google.com", "Incorrect netloc")
+            self.assertEqual(url.hostname, "www.google.com", "Incorrect hostname")
             self.assertEqual(url.port, 443, "Incorrect port")
             self.assertEqual(url.path, "/maps", "Incorrect path")
 
