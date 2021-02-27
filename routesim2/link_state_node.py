d4e4b78e5ee6022eaf135000a78b39b96f75d3c8
Tiger Nie <nhl0819@gmail.com>
2021-02-27 10:07:03 +0800

    more code

--- routesim2/link_state_node.py
+++ routesim2/link_state_node.py
@@ -1,7 +1,9 @@
-from typing import NamedTuple
+from typing import Dict, NamedTuple, List
 import json
 
 from simulator.node import Node
+
+from adj_graph import Edge, Neighbor, Graph
 
 
 # For each neighbour we need to keep track of
@@ -9,72 +11,66 @@ from simulator.node import Node
 # latency
 # seq_n <- for the update of a specific link
 
-class LinkStateBroadcastMsg(NamedTuple):
-    seq_n: int
-    latency: int
-    node1: int
-    node2: int
+
+class LinkStateBroadcastMsg(Edge):
+    pass
 
 
 class Link_State_Node(Node):
     def __init__(self, id):
         super().__init__(id)
 
-        # also keep track of neigbours seq_n
-        self.neighbors = {}  # mapping to {seq_n, latency, dest}
+        # graph - adjacency list
+        self.graph: Graph = Graph()
         
-        # adjacency list
-        # map from vertex to a list of (neighbor, latency, seq_n)
-        self.graph
-
-        # self.neighbors = self.graph[self.id]
+        # reachable - ReachableNode : NextHop
+        self.reachable: Map[int, int] = {}
 
     # Return a string
     def __str__(self):
-        return "Rewrite this function to define your node dump printout"
+        return f"<Node {self.id}>"
 
     # Fill in this function
     def link_has_been_updated(self, neighbor, latency):
 
         broadcast_msg: str = ""
 
-        if latency == -1 and neighbor in self.neighbors:
-            # latency = -1 if delete a link
-            msg_obj = LinkStateBroadcastMsg(node1=self.id, node2=neighbor, latency=latency, 
-                seq_n=self.neighbors[neighbor]["seq_n"]
-            )
-            broadcast_msg = json.dumps(msg_obj)
-            del self.neighbors[neighbor]
+        if latency == -1:
+            # delete the edge
+            e: Edge = self.graph.get_edge(self.id, neighbor)
+            broadcast_msg = json.dumps(Edge(
+                node1=self.id,
+                node2=neighbor,
+                seq_n=e.seq_n,
+                latency=-1,
+            ))
 
-        elif neighbor in self.neighbors:
-            # update an existing neighbor
-            self.neighbors[neighbor]["latency"] = latency
-            msg_obj = LinkStateBroadcastMsg(node1=self.id, node2=neighbor, latency=latency, 
-                seq_n=self.neighbors[neighbor]["seq_n"]
-            )
-            broadcast_msg = json.dumps(msg_obj)
+            self.graph.remove_edge(self.id, neighbor)
         else:
-            # new neighbors
-            self.neighbors[neighbor] = {
-                "latency": latency,
-                "dest": neighbors,
-                "seq_n": 0,
-            }
-            msg_obj = LinkStateBroadcastMsg(node1=self.id, node2=neighbor, latency=latency, 
-                seq_n=self.neighbors[neighbor]["seq_n"]
-            )
-            broadcast_msg = json.dumps(msg_obj)
+            try:
+                e: Edge = self.graph.get_edge(self.id, neighbor)
+            except ValueError:
+                # new edge
+                self.graph.add_edge(self.id, neighbor, latency, 0)
+                e: Edge = self.graph.get_edge(self.id, neighbor)
+            else:
+                # update edge
+                self.graph.update_edge(self.id, neighbor, latency, e.seq_n + 1)
+
+            broadcast_msg = json.dumps(e)
 
         # TODO: RUN DIJKSTRA
-        
+
         # BROADCAST THIS MSG
         self.send_to_neighbors(broadcast_msg)
-        self.logging.debug('link update, neighbor %d, latency %d, time %d' % (neighbor, latency, self.get_time()))
+        self.logging.debug(
+            "link update, neighbor %d, latency %d, time %d"
+            % (neighbor, latency, self.get_time())
+        )
 
     # Fill in this function
     def process_incoming_routing_message(self, m):
         msg_obj = LinkStateBroadcastMsg(*json.loads(m))
-        msg_obj.node1
         # check if node1 or node2 is in the adj list
         if msg_obj.node1 in self.neighbors:
             pass
@@ -84,8 +80,8 @@ class Link_State_Node(Node):
             # new edge
             pass
 
-        # TODO: RUN DIJKSTRA
+        # TODO: RUN DJIKSTRA
 
     # Return a neighbors, -1 if no path to destination
     def get_next_hop(self, destination):
-        return -1
+        return self.reachable.get(destination, -1)
