c7b994576d8e21a2df0db852f0286709c873af32
Tiger Nie <nhl0819@gmail.com>
2021-03-01 18:36:06 +0800

    starting dist

--- routesim2/distance_vector_node.py
+++ routesim2/distance_vector_node.py
@@ -1,18 +1,27 @@
+from typing import Dict
+
 from simulator.node import Node
 
 
 class Distance_Vector_Node(Node):
     def __init__(self, id):
         super().__init__(id)
+        # Cost and next hop for every destination
+        self.dv: Dict = {}
 
     # Return a string
     def __str__(self):
-        return "Rewrite this function to define your node dump printout"
+        return f < "Node {self.id}>"
 
     # Fill in this function
     def link_has_been_updated(self, neighbor, latency):
         # latency = -1 if delete a link
-        pass
+        if latency < 0:
+            del self.dv[neighbor]
+            # send to neighbors
+        else:
+            self.dv[neighbor] = latency
+        # send to neighbors
 
     # Fill in this function
     def process_incoming_routing_message(self, m):
